2edd299e94f4c62e2f301022226f549c19a2eb9a
Matthew Holdsworth <matthew.holdsworth@icemedia.com.au>
2018-11-28 17:24:41 +1000

    ModalDemo

--- Screens/Components/ModalDemo.js
+++ Screens/Components/ModalDemo.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { Modal, Text, View } from 'react-native';
+import { Alert, Modal, Text, TouchableHighlight, View } from 'react-native';
 
 class ModalDemo extends React.Component {
-    render() {
+    state = {
+        modalVisible: false,
+      };
+    
+      setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+      }
+    
+      render() {
         return (
-            <View>
-
-            </View>
-        )
+            <View style={{marginTop: 22}}>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                }}>
+                    <View style={{marginTop: 22}}>
+                        <View>
+                        <Text>Hello World!</Text>
+                            <TouchableHighlight
+                                onPress={() => {
+                                this.setModalVisible(!this.state.modalVisible);
+                                }}>
+                                <Text>Hide Modal</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
+            <TouchableHighlight
+                onPress={() => {
+                    this.setModalVisible(true);
+                }}>
+                <Text>Show Modal</Text>
+            </TouchableHighlight>
+          </View>
+        );
     }
     
 }
