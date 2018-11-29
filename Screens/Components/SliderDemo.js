7df2c1c3fa59245e0bc6c69a4e6dd2924be34098
Matthew Holdsworth <matthew.holdsworth@icemedia.com.au>
2018-11-29 13:21:32 +1000

    SliderDemo

--- Screens/Components/SliderDemo.js
+++ Screens/Components/SliderDemo.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Slider, Text, View } from 'react-native';
+import { Slider, StyleSheet, Text, View } from 'react-native';
 
 class SliderDemo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 50,
+        };
+    }
+    
+    change(value) {
+        this.setState(() => {
+            return {
+                value: parseFloat(value),
+            };
+        });
+    }
+    
     render() {
+        const {value} = this.state;
         return (
-            <View>
-
+          <View style={styles.container}>
+            <Text style={styles.text}>{String(value)}</Text>
+                <Slider
+                    step={1}
+                    maximumValue={100}
+                    onValueChange={this.change.bind(this)}
+                    value={value}
+                />
             </View>
         )
     }
-    
 }
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 50,
+        textAlign: 'center',
+    },
+})
+
 export default SliderDemo
